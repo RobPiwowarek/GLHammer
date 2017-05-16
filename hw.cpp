c7889eaf4c92930296ea2bd5d4b50f9fc399d5cb
Robert Piwowarek <robpiwowarek@gmail.com>
2017-05-16 10:48:18 +0200

    Drawing a single triangle
    hello world

---
+++ hw.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <SOIL.h>
+#include <glm.hpp>
+
+#include "shaderManager.h"
+
+using namespace std;
+
+int main(){
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+
+	glViewport(0, 0, width, height);
+
+	// verts
+
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	// VBO
+
+	GLuint VBO;
+	glGenBuffers(1, &VBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// shaders
+
+	ShaderManager theProgram("main.vert", "main.frag");
+
+	//
+
+	// 0. Copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 1. Then set the vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// 2. Use our shader program when we want to render an object
+	theProgram.Use();
+	// 3. Now draw the object 
+
+	GLuint VAO;
+	glGenVertexArrays(1, &VAO);
+
+	// ..:: Initialization code (done once (unless your object frequently changes)) :: ..
+	// 1. Bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// 2. Copy our vertices array in a buffer for OpenGL to use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// 3. Then set our vertex attributes pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	//4. Unbind the VAO
+	glBindVertexArray(0);
+
+
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+
+		// actual drawing
+
+		theProgram.Use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
+
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
---
+++ shaderManager.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class ShaderManager
+{
+	GLuint program_id; 	// The program ID
+public:
+	// Constructor reads and builds the shader
+	ShaderManager(const GLchar* vertexPath, const GLchar* fragmentPath);
+
+	// Use the program
+	void Use() const
+	{
+		glUseProgram(get_programID());
+	}
+
+	// returns program ID
+	GLuint get_programID() const
+	{
+		return program_id;
+	}
+};
